4b66fabb5c3334b2919f1319dc86a6ee63420d86
Diego Delgado Barra <diego.delgadobarra@outlook.com>
2022-06-21 22:21:07 -0400

    LastCommit;Finished

--- index.js
+++ index.js
@@ -21,3 +21,43 @@ app.post("/usuario", async (req, res) => {
         res.status(500).json({error: "Ha ocurrido un error en el servidor"});
     }
 });
+
+// /usuario PUT: Recibe los datos modificados de un usuario registrado y los actualiza.
+
+app.put("/usuario", async (req, res) => {
+    let id = req.query.id
+    try {
+        const resp = await actualizarUsuario(id, req.body);
+        res.status(201).json(resp.rows ? resp : { code: resp.code})
+    } catch (error) {
+        res.status(500).json({error: "Ha ocurrido un error en el servidor"});
+    }
+});
+
+// /usuario DELETE: Recibe el id de un usuario registrado y lo elimina.
+
+app.delete("/usuario", async (req, res) => {
+    const id = req.query.id;
+    const resp = await eliminarUsuario(id);
+    res.json(resp.rows ? resp : { code: resp.code})
+});
+
+// /transferencia POST: Recibe los datos para realizar una nueva transferencia.
+// Se debe ocupar una transacción SQL en la base de datos.
+
+app.post("/transferencia", async (req, res) => {
+    try {
+        const resp = await insertarTransferencia(req.body);
+        res.status(201).json(resp.rows ? resp : { code:resp.code})
+    } catch (error) {
+        res.status(500).json({error: "Ha ocurrido un error en el servidor"});
+    }
+});
+
+// /transferencias GET: Devuelve todas las transferencias almacenadas en la base de
+// datos en formato de arreglo.
+
+app.get("/transferencias", async (req, res) => {
+    const resp = await consultarTransferencias();
+    res.json(resp.rows);
+});
